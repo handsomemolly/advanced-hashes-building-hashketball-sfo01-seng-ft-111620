5a0eca8c2af732a72122399c477ea9329a71f5f4
Ryan Kendig <ryan.kendig@gmail.com>
2020-11-05 18:16:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,7 +2,7 @@
 def game_hash
   {
     :home_team => {
-      :team_name => ["Brooklyn Nets"],
+      :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
       :players => [
         {
@@ -63,7 +63,7 @@ def game_hash
       ]
     },
     :away_team => {
-      :team_name => ["Charlotte Hornets"],
+      :team_name => "Charlotte Hornets",
       :colors => ["Turquoise", "Purple"],
       :players => [
         {
