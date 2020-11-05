3af5ff120991e6ac22e327a665c334d7412c46c5
Ryan Kendig <ryan.kendig@gmail.com>
2020-11-05 17:48:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -8,7 +8,16 @@ def game_hash
         {
           :name => "Alan Anderson",
           :number => 0,
-          :shoe => 16
+          :shoe => 16,
+          :points => 22,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 1
+        },
+        {
+          
         }
         ]
     }
