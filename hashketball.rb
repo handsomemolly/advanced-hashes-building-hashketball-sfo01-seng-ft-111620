ca124ff2226fa09fbfeac0aac31848440b4679f6
Ryan Kendig <ryan.kendig@gmail.com>
2020-11-05 17:46:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,16 @@
 # Write your code here!
 def game_hash
-  
+  {
+    :home_team => {
+      :name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
+      :players => [
+        {
+          :name => "Alan Anderson",
+          :number => 0,
+          :shoe => 16
+        }
+        ]
+    }
+  }
   
